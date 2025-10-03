e7f015a1273d08c0ac1d672a757e3f084eb6ea5f
dAnielDark884 <danielcifuentes1506@gmail.com>
2025-10-03 13:31:12 -0500

    Add files via upload

---
+++ Corte1C++/do_while/sketch_oct3f.ino
@@ -0,0 +1,20 @@
+const int ledPin = 2;
+
+void setup() {
+  pinMode(ledPin,2);
+
+}
+
+void loop() {
+  int blinkCounter = 0;
+
+  do{
+    digitalWrite(2,HIGH);
+    delay(200);
+    digitalWrite(2,LOW);
+    delay(200);
+    blinkCounter++;
+  }while (blinkCounter < 5);
+  delay(2000);
+
+}
