f0c1b0cb1bbe097fc41acd6e8077225ca1272bbf
dAnielDark884 <danielcifuentes1506@gmail.com>
2025-10-05 17:24:03 -0500

    Create main.Cpp1.cpp

---
+++ Corte1C++/Proyecto 1/main.Cpp1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+//I want to learn how to code in C++
+
+int main(int argc, char** argv) {
+	
+	std::cout << "HELLO WORLD" << std::endl;
+	
+	return 0;
+}
