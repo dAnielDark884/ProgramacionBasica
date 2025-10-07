7d5540529464378dbb7bbd64bfde6c339f20c0a1
dAnielDark884 <danielcifuentes1506@gmail.com>
2025-10-07 18:18:23 -0500

    Add files via upload

---
+++ Corte1C++/do_while/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+using namespace std;
+int main(int argc, char** argv) {
+	int i= 0;
+	
+	do {
+		cout<<"valor de i:"<<i<<endl;
+		i++;
+	}while(i < 5);
+	
+	
+	return 0;
+}
