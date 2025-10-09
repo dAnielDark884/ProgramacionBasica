847da1e91622ccca385db38650f321f80c60111b
dAnielDark884 <danielcifuentes1506@gmail.com>
2025-10-09 13:00:23 -0500

    Add files via upload

---
+++ Corte1C/Arrays_loops/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int myNumbers[]={25,50,70,100};
+	int i;
+	for(i=0;i<4;i++)
+	print("%d",i);
+	return 0;
+}
