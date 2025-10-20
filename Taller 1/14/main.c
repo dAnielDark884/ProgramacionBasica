f90b2b89d3cb903b6dee27dc439bf0e90eacb19d
dAnielDark884 <danielcifuentes1506@gmail.com>
2025-10-20 16:54:09 -0500

    Add main.c to count positive and negative numbers
    Implement a program to count positive and negative numbers.

---
+++ Taller 1/14/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int num, positivos = 0, negativos = 0;
+
+    printf("Ingrese números (0 para terminar):\n");
+    while (1) {
+        scanf("%d", &num);
+        if (num == 0)
+            break;
+        if (num > 0)
+            positivos++;
+        else
+            negativos++;
+    }
+
+    printf("Positivos: %d\nNegativos: %d\n", positivos, negativos);
+    return 0;
+}
